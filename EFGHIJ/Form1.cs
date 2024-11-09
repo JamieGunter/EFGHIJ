0e58191fbd727e37fa335eb4070866bbe40efb1e
JamieGunter <Jamieshichang@gmail.com>
2024-11-09 16:23:07 +0000

    Buttons added to test form

--- EFGHIJ/Form1.cs
+++ EFGHIJ/Form1.cs
@@ -15,5 +15,30 @@ namespace EFGHIJ
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
