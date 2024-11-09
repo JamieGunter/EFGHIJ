aac45da5fdd146d5c4a31fdc3e6456784694e808
JamieGunter <Jamieshichang@gmail.com>
2024-11-09 17:56:51 +0000

    Buttons + Tracker Slider + Controller Compatability completed

--- EFGHIJ/Form1.cs
+++ EFGHIJ/Form1.cs
@@ -6,39 +6,81 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using SharpDX.DirectInput;
+using SharpDX.XInput;
+using System.Threading;
+
 
 namespace EFGHIJ
 {
-    public partial class Form1 : Form
+    public partial class vibrationDebugMenu : Form
     {
-        public Form1()
+        private DirectInput directInput;
+        private Joystick joystick;
+        private Controller controller;
+        public vibrationDebugMenu()
         {
             InitializeComponent();
+            InitializeController();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void InitializeController()
         {
-
+            controller = new Controller(UserIndex.One);
+            if (controller.IsConnected)
+            {
+                MessageBox.Show("Controller is connected.");
+            }
+            else
+            {
+                MessageBox.Show("No controller found.");
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void SetVibration(int leftMotor, int rightMotor)
         {
-
+            if (controller != null && controller.IsConnected)
+            {
+                var vibration = new Vibration
+                {
+                    LeftMotorSpeed = (ushort)leftMotor,
+                    RightMotorSpeed = (ushort)rightMotor
+                };
+                controller.SetVibration(vibration);
+            }
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void OffButton_Click(object sender, EventArgs e)
         {
-
+            SetVibration(0,0);
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void QuarterPercent_Click(object sender, EventArgs e)
         {
-
+            SetVibration(16383, 16383);
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void FiftyPercent_Click(object sender, EventArgs e)
         {
+            SetVibration(32767, 32767);
+        }
 
+        private void ThreeQuarterPercent_Click(object sender, EventArgs e)
+        {
+            SetVibration(49151, 49151);
+        }
+
+        private void MaxPercent_Click(object sender, EventArgs e)
+        {
+            SetVibration(65535, 65535);
+        }
+
+        private void vibrationTrackBar_Scroll(object sender, EventArgs e)
+        {
+            int vibrationValue = vibrationTrackBar.Value;
+            vibrationLabel.Text = vibrationValue.ToString();
+            int scaledValue = (int)(vibrationValue * 655.35);
+            SetVibration(scaledValue, scaledValue);
         }
     }
 }
--- EFGHIJ/Program.cs
+++ EFGHIJ/Program.cs
@@ -15,7 +15,7 @@ namespace EFGHIJ
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new vibrationDebugMenu());
         }
     }
 }
