8b09110cf4c671866a0a68b1483ee615bd6b0256
Jamie Gunter <JamieGunter@users.noreply.github.com>
2025-01-14 18:19:40 +0000

    Added newly created controller glyph images, added vanilla instructions, incorporated new glyph symbol images into vanilla interface.

--- EFGHIJ/VanillaForm.Designer.cs
+++ EFGHIJ/VanillaForm.Designer.cs
@@ -28,15 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VanillaForm));
             this.getOriginalStimuliButton = new System.Windows.Forms.Button();
             this.getNewStimuliButton = new System.Windows.Forms.Button();
             this.V2IsLowerButton = new System.Windows.Forms.Button();
             this.V2IsNotLowerButton = new System.Windows.Forms.Button();
             this.trialNumberLabel = new System.Windows.Forms.Label();
             this.taskInitiatorGridBox = new System.Windows.Forms.GroupBox();
-            this.beginTaskButton = new System.Windows.Forms.Button();
+            this.VanillaSubheadingLabel = new System.Windows.Forms.Label();
+            this.VanillaTitleLabel = new System.Windows.Forms.Label();
             this.instructionLabel = new System.Windows.Forms.Label();
+            this.beginTaskButton = new System.Windows.Forms.Button();
+            this.VanillaTaskNumberTitle = new System.Windows.Forms.Label();
+            this.VanillaUpDpadGlyphImage = new System.Windows.Forms.PictureBox();
+            this.VanillaDownDpadGlyphImage = new System.Windows.Forms.PictureBox();
             this.taskInitiatorGridBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.VanillaUpDpadGlyphImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VanillaDownDpadGlyphImage)).BeginInit();
             this.SuspendLayout();
             // 
             // getOriginalStimuliButton
@@ -45,7 +53,7 @@
             this.getOriginalStimuliButton.Name = "getOriginalStimuliButton";
             this.getOriginalStimuliButton.Size = new System.Drawing.Size(149, 63);
             this.getOriginalStimuliButton.TabIndex = 0;
-            this.getOriginalStimuliButton.Text = "Get Original Stimuli Reference";
+            this.getOriginalStimuliButton.Text = "Get Original Stimuli Reference (V1)";
             this.getOriginalStimuliButton.UseVisualStyleBackColor = true;
             this.getOriginalStimuliButton.Click += new System.EventHandler(this.getOriginalStimuliButton_Click);
             // 
@@ -55,13 +63,13 @@
             this.getNewStimuliButton.Name = "getNewStimuliButton";
             this.getNewStimuliButton.Size = new System.Drawing.Size(149, 63);
             this.getNewStimuliButton.TabIndex = 1;
-            this.getNewStimuliButton.Text = "Get New Stimuli Reference";
+            this.getNewStimuliButton.Text = "Get New Stimuli Reference (V2)";
             this.getNewStimuliButton.UseVisualStyleBackColor = true;
             this.getNewStimuliButton.Click += new System.EventHandler(this.getNewStimuliButton_Click);
             // 
             // V2IsLowerButton
             // 
-            this.V2IsLowerButton.Location = new System.Drawing.Point(216, 185);
+            this.V2IsLowerButton.Location = new System.Drawing.Point(226, 194);
             this.V2IsLowerButton.Name = "V2IsLowerButton";
             this.V2IsLowerButton.Size = new System.Drawing.Size(149, 63);
             this.V2IsLowerButton.TabIndex = 2;
@@ -71,7 +79,7 @@
             // 
             // V2IsNotLowerButton
             // 
-            this.V2IsNotLowerButton.Location = new System.Drawing.Point(407, 185);
+            this.V2IsNotLowerButton.Location = new System.Drawing.Point(426, 194);
             this.V2IsNotLowerButton.Name = "V2IsNotLowerButton";
             this.V2IsNotLowerButton.Size = new System.Drawing.Size(149, 63);
             this.V2IsNotLowerButton.TabIndex = 3;
@@ -83,7 +91,7 @@
             // 
             this.trialNumberLabel.AutoSize = true;
             this.trialNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
-            this.trialNumberLabel.Location = new System.Drawing.Point(365, 127);
+            this.trialNumberLabel.Location = new System.Drawing.Point(379, 127);
             this.trialNumberLabel.Name = "trialNumberLabel";
             this.trialNumberLabel.Size = new System.Drawing.Size(42, 46);
             this.trialNumberLabel.TabIndex = 4;
@@ -92,17 +100,54 @@
             // 
             // taskInitiatorGridBox
             // 
+            this.taskInitiatorGridBox.Controls.Add(this.VanillaSubheadingLabel);
+            this.taskInitiatorGridBox.Controls.Add(this.VanillaTitleLabel);
             this.taskInitiatorGridBox.Controls.Add(this.instructionLabel);
             this.taskInitiatorGridBox.Controls.Add(this.beginTaskButton);
-            this.taskInitiatorGridBox.Location = new System.Drawing.Point(0, 0);
+            this.taskInitiatorGridBox.Location = new System.Drawing.Point(1, -6);
             this.taskInitiatorGridBox.Name = "taskInitiatorGridBox";
-            this.taskInitiatorGridBox.Size = new System.Drawing.Size(802, 453);
+            this.taskInitiatorGridBox.Size = new System.Drawing.Size(802, 457);
             this.taskInitiatorGridBox.TabIndex = 5;
             this.taskInitiatorGridBox.TabStop = false;
             // 
+            // VanillaSubheadingLabel
+            // 
+            this.VanillaSubheadingLabel.AutoSize = true;
+            this.VanillaSubheadingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VanillaSubheadingLabel.Location = new System.Drawing.Point(108, 45);
+            this.VanillaSubheadingLabel.Name = "VanillaSubheadingLabel";
+            this.VanillaSubheadingLabel.Size = new System.Drawing.Size(587, 40);
+            this.VanillaSubheadingLabel.TabIndex = 3;
+            this.VanillaSubheadingLabel.Text = "Using either the Directional Pad (DPad) on the left side of the controller,\r\nor t" +
+    "he mouse to click the buttons, please perform the following task:";
+            this.VanillaSubheadingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // VanillaTitleLabel
+            // 
+            this.VanillaTitleLabel.AutoSize = true;
+            this.VanillaTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.VanillaTitleLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.VanillaTitleLabel.Location = new System.Drawing.Point(97, 9);
+            this.VanillaTitleLabel.Name = "VanillaTitleLabel";
+            this.VanillaTitleLabel.Size = new System.Drawing.Size(609, 29);
+            this.VanillaTitleLabel.TabIndex = 2;
+            this.VanillaTitleLabel.Text = "Welcome to the Vanilla Interface of this experiment.";
+            this.VanillaTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.instructionLabel.Location = new System.Drawing.Point(83, 93);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(637, 280);
+            this.instructionLabel.TabIndex = 1;
+            this.instructionLabel.Text = resources.GetString("instructionLabel.Text");
+            this.instructionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // beginTaskButton
             // 
-            this.beginTaskButton.Location = new System.Drawing.Point(289, 296);
+            this.beginTaskButton.Location = new System.Drawing.Point(295, 379);
             this.beginTaskButton.Name = "beginTaskButton";
             this.beginTaskButton.Size = new System.Drawing.Size(212, 67);
             this.beginTaskButton.TabIndex = 0;
@@ -110,15 +155,36 @@
             this.beginTaskButton.UseVisualStyleBackColor = true;
             this.beginTaskButton.Click += new System.EventHandler(this.beginTaskButton_Click);
             // 
-            // instructionLabel
+            // VanillaTaskNumberTitle
             // 
-            this.instructionLabel.AutoSize = true;
-            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.instructionLabel.Location = new System.Drawing.Point(305, 156);
-            this.instructionLabel.Name = "instructionLabel";
-            this.instructionLabel.Size = new System.Drawing.Size(176, 20);
-            this.instructionLabel.TabIndex = 1;
-            this.instructionLabel.Text = "Insert Instructions Here";
+            this.VanillaTaskNumberTitle.AutoSize = true;
+            this.VanillaTaskNumberTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VanillaTaskNumberTitle.Location = new System.Drawing.Point(343, 101);
+            this.VanillaTaskNumberTitle.Name = "VanillaTaskNumberTitle";
+            this.VanillaTaskNumberTitle.Size = new System.Drawing.Size(114, 20);
+            this.VanillaTaskNumberTitle.TabIndex = 6;
+            this.VanillaTaskNumberTitle.Text = "Task Number";
+            this.VanillaTaskNumberTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // VanillaUpDpadGlyphImage
+            // 
+            this.VanillaUpDpadGlyphImage.BackColor = System.Drawing.Color.Transparent;
+            this.VanillaUpDpadGlyphImage.Location = new System.Drawing.Point(570, 12);
+            this.VanillaUpDpadGlyphImage.Name = "VanillaUpDpadGlyphImage";
+            this.VanillaUpDpadGlyphImage.Size = new System.Drawing.Size(63, 63);
+            this.VanillaUpDpadGlyphImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.VanillaUpDpadGlyphImage.TabIndex = 7;
+            this.VanillaUpDpadGlyphImage.TabStop = false;
+            // 
+            // VanillaDownDpadGlyphImage
+            // 
+            this.VanillaDownDpadGlyphImage.BackColor = System.Drawing.Color.Transparent;
+            this.VanillaDownDpadGlyphImage.Location = new System.Drawing.Point(570, 96);
+            this.VanillaDownDpadGlyphImage.Name = "VanillaDownDpadGlyphImage";
+            this.VanillaDownDpadGlyphImage.Size = new System.Drawing.Size(63, 63);
+            this.VanillaDownDpadGlyphImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.VanillaDownDpadGlyphImage.TabIndex = 8;
+            this.VanillaDownDpadGlyphImage.TabStop = false;
             // 
             // VanillaForm
             // 
@@ -126,6 +192,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.taskInitiatorGridBox);
+            this.Controls.Add(this.VanillaDownDpadGlyphImage);
+            this.Controls.Add(this.VanillaUpDpadGlyphImage);
+            this.Controls.Add(this.VanillaTaskNumberTitle);
             this.Controls.Add(this.trialNumberLabel);
             this.Controls.Add(this.V2IsNotLowerButton);
             this.Controls.Add(this.V2IsLowerButton);
@@ -135,6 +204,8 @@
             this.Text = "Vanilla Interface";
             this.taskInitiatorGridBox.ResumeLayout(false);
             this.taskInitiatorGridBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.VanillaUpDpadGlyphImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VanillaDownDpadGlyphImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,5 +221,10 @@
         private System.Windows.Forms.GroupBox taskInitiatorGridBox;
         private System.Windows.Forms.Label instructionLabel;
         private System.Windows.Forms.Button beginTaskButton;
+        private System.Windows.Forms.Label VanillaSubheadingLabel;
+        private System.Windows.Forms.Label VanillaTitleLabel;
+        private System.Windows.Forms.Label VanillaTaskNumberTitle;
+        private System.Windows.Forms.PictureBox VanillaUpDpadGlyphImage;
+        private System.Windows.Forms.PictureBox VanillaDownDpadGlyphImage;
     }
 }
